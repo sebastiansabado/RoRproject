e6d1a753680594a46e555b9350cff5aeae833fc9
Sebastian Sabado <sebastiansabado2@yahoo.com>
2017-03-15 01:13:45 +0000

    Rails 5.4

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,10 @@ Rails.application.routes.draw do
 
   get 'favorites/movies'
 
-  root 'static_pages#home'
-  
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-  
-  get 'static_pages/about'
-  
-  get 'static_pages/contact'
+   root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
