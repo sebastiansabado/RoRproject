7973bfcba1ad7ab3d29c1ddd3f277cfcf00edb62
Sebastian Sabado <sebastiansabado2@yahoo.com>
2017-04-12 02:24:46 +0000

    Working chapter 6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412013706) do
+ActiveRecord::Schema.define(version: 20170412001719) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
