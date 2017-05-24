44f9c6e642fbd4b827b420d41cd0d90ba63dc82e
Sebastian Sabado <sebastiansabado2@yahoo.com>
2017-05-24 00:44:14 +0000

    Finish user edit, update, index, and destroy actions work on 10.56

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522225954) do
+ActiveRecord::Schema.define(version: 20170524001651) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "name"
@@ -32,9 +32,10 @@ ActiveRecord::Schema.define(version: 20170522225954) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
