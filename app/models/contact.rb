68745cf21d0aac76779975fccd71c565019ce934
Sebastian Sabado <sebastiansabado2@yahoo.com>
2017-06-07 02:56:34 +0000

    Rails.env Correction

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,9 @@
 class Contact < ApplicationRecord
   def self.search(search)
-  where("last_name ILIKE ? OR phone_number ILIKE ? OR first_name ILIKE ? OR email ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
+    if Rails.env.production?
+      where("last_name ILIKE ? OR phone_number ILIKE ? OR first_name ILIKE ? OR email ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+    else
+       where("last_name LIKE ? OR phone_number LIKE ? OR first_name LIKE ? OR email LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+    end
   end
 end
