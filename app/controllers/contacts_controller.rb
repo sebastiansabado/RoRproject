58e94c903c4383d813a40e25c400dcc48227288f
Sebastian Sabado <sebastiansabado2@yahoo.com>
2017-06-07 00:40:00 +0000

    case sensative bug

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
       end
       
       if @contacts.blank? 
-       flash[:danger] = " There was no #{params[:search]} result" 
+       flash.now[:danger] = " There was no #{params[:search]} result" 
       end 
   end
   
@@ -19,7 +19,7 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.create(params.require(:contact).permit(:last_name.downcase, :first_name.downcase, :email.downcase, :phone_number))
-      flash[:success] = "The Contact \"#{@contact.first_name} #{@contact.last_name} \" has been created Successfully!"
+      flash.now[:success] = "The Contact \"#{@contact.first_name} #{@contact.last_name} \" has been created Successfully!"
       redirect_to contacts_path
   end
 
@@ -34,14 +34,14 @@ class ContactsController < ApplicationController
   def destroy
     @contact = Contact.find(params[:id])
     @contact.destroy
-    flash[:success] = "The Contact \"#{@contact.first_name} #{@contact.last_name} \" has been deleted successfully!"
+    flash.now[:success] = "The Contact \"#{@contact.first_name} #{@contact.last_name} \" has been deleted successfully!"
     redirect_to contacts_path
   end
 
   def update
     @contact = Contact.find(params[:id])
 	  @contact.update(params.require(:contact).permit(:last_name, :first_name, :email, :phone_number))
-	  flash[:success] = "The Contact \"#{@contact.first_name} #{@contact.last_name} \" has been edited successfully!"
+	  flash.now[:success] = "The Contact \"#{@contact.first_name} #{@contact.last_name} \" has been edited successfully!"
 	 redirect_to contacts_path
   end
 end
