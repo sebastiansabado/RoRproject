aa7e51ada8ff9afbecc0bb113ee8c097edbbc226
Sebastian Sabado <sebastiansabado2@yahoo.com>
2017-06-07 00:26:57 +0000

    finish Case-Sensitivity

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -18,7 +18,7 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.create(params.require(:contact).permit(:last_name, :first_name, :email, :phone_number))
+    @contact = Contact.create(params.require(:contact).permit(:last_name.downcase, :first_name.downcase, :email.downcase, :phone_number))
       flash[:success] = "The Contact \"#{@contact.first_name} #{@contact.last_name} \" has been created Successfully!"
       redirect_to contacts_path
   end
