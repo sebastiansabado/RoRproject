c000ee224d91000ee4d96af9d9c435dbb6788e1c
Sebastian Sabado <sebastiansabado2@yahoo.com>
2017-06-07 00:52:25 +0000

    New bug

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
   def self.search(search)
-  where("last_name LIKE ? OR phone_number LIKE ? OR first_name LIKE ? OR email LIKE ?", "%#{search.downcase}%", "%#{search}%", "%#{search.downcase}%", "%#{search.downcase}%") 
+  where("last_name LIKE ? OR phone_number LIKE ? OR first_name LIKE ? OR email LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
   end
 end
